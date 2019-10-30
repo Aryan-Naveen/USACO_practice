c4e8dc1ec2900dfad659c46ccf571679f88a7367
Aryan-Naveen <36176381+Aryan-Naveen@users.noreply.github.com>
2019-10-30 17:18:33 -0400

    Add files via upload

--- friday.py
+++ friday.py
@@ -19,61 +19,59 @@ class year():
     def __init__(self, startDay, i):
         self.i = i
         self.startDay = startDay
-        self.leapYear = (i%100 == 0 and i%400 == 0) or (not i%100 == 0 and i%4 == 0)
-        print(self.leapYear) 
+        self.leapYear = (i%100 == 0 and (1900 + i) %400 == 0) or (not i%100 == 0 and i%4 == 0)
     
     def get_next_year_startDay(self):
-        if(self.leapYear):
-            return (self.startDay + 366) % 7
-        else:
-            return (self.startDay + 365) % 7
+        return self.start
 
     def get_thirteen_count(self):
-        months_with_30_days = [3, 5, 9, 10]
+        months_with_30_days = [3, 5, 8, 10]
 
         thirteen_count = [0, 0, 0, 0, 0, 0, 0]
 
-        start = self.startDay
+        self.start = self.startDay
         
         for i in range(12):
             length = 0
             if(i in months_with_30_days):
                 length = 30
             elif(i == 1):
-                if(self.leapYear and not self.i==0):
+                if(self.leapYear):
                     length = 29
                 else:
-                    print("safd")
                     length = 28
             else:
                 length = 31
             
-            Month = month(start, length)
+            Month = month(self.start, length)
             day = Month.get_day_13th_occurs()
             thirteen_count[day] += 1
             
-            start = Month.get_first_day_of_next_month()
-            print(start)
+            self.start = Month.get_first_day_of_next_month()
 
         return thirteen_count
 
-# fin = open("friday.in", 'r')
-# input = int(fin.read())
-# fin.close()
+fin = open("friday.in", 'r')
+input = int(fin.read())
+fin.close()
 
 thirteen_count = [0, 0, 0, 0, 0, 0, 0]
 start = 2
-for i in range(1):
-    year = year(start, i)
-    thirteen_count_year = year.get_thirteen_count()
+for i in range(input):
+    yearObj = year(start, i)
+    thirteen_count_year = yearObj.get_thirteen_count()
     for i in range(7):
         thirteen_count[i] += thirteen_count_year[i]
-    start = year.get_next_year_startDay()
+    start = yearObj.get_next_year_startDay()
 
 fout = open("friday.out", 'w')
 output = ""
 
-for count in thirteen_count:
-    output += str(count) + " "
+for i in range(6):
+    output += str(thirteen_count[i]) + " "
+
+output += str(thirteen_count[6])
+
+print(output)
 
 fout.write(output + "\n")
