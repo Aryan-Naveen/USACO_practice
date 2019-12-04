82b8b0e1013c87207bf19e5957be2c4bd534c584
Aryan-Naveen <36176381+Aryan-Naveen@users.noreply.github.com>
2019-12-04 09:31:27 -0500

    Add files via upload

---
+++ templateUSACO.py
@@ -0,0 +1,39 @@
+"""
+ID: Aryan Naveen
+LANG: PYTHON2
+TASK: 
+"""
+
+testing = False
+num_test_files = 0
+task_name = ""
+
+def task(input):
+    #Write code for task in here
+
+def verify(output, i):
+    file = 'data/' + task_name + '/' + str(i + 1) + '.out'
+    solution = file.read().splitlines()
+    file.close()
+    return output == solution
+
+if(testing):
+    count = 0
+    for i in range(num_test_files):
+        file = 'data/' + task_name + '/' + str(i + 1) + '.in'
+        fin = open(file, 'r')
+        input = fin.read().splitlines()
+        output = task(input)
+        if(verify(output, i)):
+            count += 1
+    print("The percentage of correct answers was " + str((count/num_test_files)*100) + "%")
+
+else:
+    file = task_name + ".in"
+    fin = open(file, 'r')
+    output = task(fin.read().splitlines())
+    fin.close()
+
+    fout = task_name + '.out'
+    fout.write(output + '\n')
+    fout.close()
